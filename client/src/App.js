fad0740ca75e198bd3289464b2e5383d3be76802
kevtran2 <kevin.tran.675@gmail.com>
2025-03-13 00:39:27 -0700

    Added streamed chatgpt api and scroll to bottom

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,52 @@
 import './App.css';
-import { useState } from 'react';
-
-const sendTextMessage = async (msg) => {
-  try {
-    const response = await fetch('http://localhost:8080', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt: `My question to chatgpt: ${msg}` }),
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP POST error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log(data);
-    console.log('POST success');
-  } catch (error) {
-    console.error(`Error with POST request: ${error}`);
-  }
-}
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
   const [textBarValue, setTextBarValue] = useState('');
+  const [messageHistory, setMessageHistory] = useState([]);
+  const messageDisplayRef = useRef(null);
   
+  const sendTextMessage = async (msg) => {
+    if (msg === '') return
+    try {
+      const response = await fetch('http://localhost:8080', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: msg }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP POST error! Status: ${response.status}`);
+      }
+
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();
+
+      let streamData = '';
+      while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          streamData += decoder.decode(value);
+          if (messageHistory.length >= 6) {
+            setMessageHistory([...messageHistory.slice(2), msg, streamData])
+          } else {
+            setMessageHistory([...messageHistory, msg, streamData])
+          }
+      }
+      
+      setTextBarValue('');
+    } catch (error) {
+      console.error(`Error with POST request: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    if (messageDisplayRef.current) {
+      messageDisplayRef.current.scrollTop = messageDisplayRef.current.scrollHeight;
+    }
+  }, [messageHistory]);
+
   const handleKeyPressed = (KeyboardEvent) => {
     if (KeyboardEvent.key === 'Enter') {
       sendTextMessage(textBarValue)
@@ -36,16 +59,29 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='App-body'>
+        <h1>Chat with GPT</h1>
+        <div className='message-display scrollable' ref={messageDisplayRef}>
+          <ul>
+            {messageHistory.map((msg, i) => (
+              <li className={`${i % 2 === 0 ? "message right" : "message left"}`} 
+              key={i}>{msg}</li>
+            )
+            )}
+          </ul>
+        </div>
         <div className="text-bar" >
           <input 
           type='text' 
           onChange={handleTextState}
-          onKeyDown={handleKeyPressed}>
+          onKeyDown={handleKeyPressed}
+          value={textBarValue}
+          >
           </input>
           <button onClick={() => {sendTextMessage(textBarValue)}}>Send</button>
         </div>
-      </header>
+      </div>
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const OpenAI = require('openai');
 
 app.use(cors());
 app.use(express.json());
@@ -9,11 +11,33 @@ app.get('/', (req, res) => {
     res.json({message: 'Successfully called server GET request'})
 });
 
-app.post('/', (req, res) => {
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // note to Lei: please provide a .env variable with your own OPENAI_API_KEY
+});
+
+app.post('/', async (req, res) => {
     const reqBody = req.body.prompt;
-    res.json({message: `hellooo POST ${reqBody}`});
+    const stream = await openai.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+            {
+                role: "user",
+                content: reqBody,
+            }
+        ],
+        stream: true,
+    });
+    for await (const chunk of stream) {
+        res.write(chunk.choices[0]?.delta?.content || '');
+    }
+    res.end('');
 })
 
 app.listen(8080, () => {
     console.log('server listening on 8080')
 });
+
+
+
+
+
